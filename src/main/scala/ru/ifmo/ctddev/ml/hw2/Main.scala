be6d738e4c45e590ffa044acf47088fe9d219f29
Daniyar Itegulov <ditegulov@gmail.com>
2016-10-02 22:44:27 +0300

    Implement main with UI

--- src/main/scala/ru/ifmo/ctddev/ml/hw2/Main.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw2/Main.scala
@@ -15,17 +15,28 @@ object Main {
     val (data, means, sigmas) = Data.normalize(houses.map(_.toData))
     val geneticCoefficients = GeneticAlgorithm.fit(Seq(0, 0), Utils.mseLoss(data))
     println(s"Coefficients from genetic algorithm: ${geneticCoefficients.mkString(", ")}")
-    val geneticPredictor = LinearRegression(Data.unnormalize(geneticCoefficients, means, sigmas))
+    val geneticUnnormalizedCoefficients = Data.unnormalize(geneticCoefficients, means, sigmas)
+    println(s"Unnormalized coefficients from genetic algorithm: ${geneticUnnormalizedCoefficients.mkString(", ")}")
+    val geneticPredictor = LinearRegression(geneticUnnormalizedCoefficients)
 
     val gradientCoefficients = GradientDescent.fit(Seq(0, 0), Utils.mseLoss(data))
     println(s"Coefficients from gradient algorithm: ${gradientCoefficients.mkString(", ")}")
     val gradientPredictor = LinearRegression(Data.unnormalize(geneticCoefficients, means, sigmas))
 
+    val areaFigure = Figure()
+    val areaPlot = areaFigure.subplot(0)
+    areaPlot += scatter(houses.map(_.house.area), houses.map(_.price), _ => 20.0, colors = _ => Color.red)
+    areaPlot.title = "Area"
+
+    val l = linspace(0, 5000.0)
+    areaPlot += plot(l, l.map(_ * geneticCoefficients.head), '.')
+    areaPlot += plot(l, l.map(_ * gradientCoefficients.head), '.')
+
     while (true) {
       val area = StdIn.readInt()
       val rooms = StdIn.readInt()
       println("Genetic-predicted price: " + geneticPredictor(Seq(area, rooms)))
-      println("Gradient-predicted price: " + gradientPredictor(Seq(area, rooms)))
+//      println("Gradient-predicted price: " + gradientPredictor(Seq(area, rooms)))
     }
   }
 }
