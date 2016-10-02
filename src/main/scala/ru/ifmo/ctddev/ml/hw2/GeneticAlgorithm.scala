3f2f2119772fb33252529db1d5c595d019b98a07
Daniyar Itegulov <ditegulov@gmail.com>
2016-10-02 21:09:39 +0300

    Implement Genetic Algo

--- src/main/scala/ru/ifmo/ctddev/ml/hw2/GeneticAlgorithm.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw2/GeneticAlgorithm.scala
@@ -1,7 +1,20 @@
 package ru.ifmo.ctddev.ml.hw2
 
+import scala.util.Random
+
 object GeneticAlgorithm extends LinearSolver {
-  override def solve(dataSet: Seq[Data]): Seq[Double] = {
-    ???
+  override def solve(dataSet: Seq[Data],
+                     initialCoefficients: Seq[Double],
+                     loss: Seq[Double] => Double): Seq[Double] = {
+    var brood = Seq(initialCoefficients)
+    for (i <- 1 to 20) {
+      val newBrood = for (
+        individual <- brood;
+        times <- 1 to 6
+      ) yield individual.map(_ + (Random.nextDouble() * 2 - 1))
+
+      brood = newBrood.sortBy(loss).take(50)
+    }
+    brood.minBy(loss)
   }
 }
--- src/main/scala/ru/ifmo/ctddev/ml/hw2/LinearSolver.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw2/LinearSolver.scala
@@ -1,5 +1,7 @@
 package ru.ifmo.ctddev.ml.hw2
 
 trait LinearSolver {
-  def solve(dataSet: Seq[Data]): Seq[Double]
+  def solve(dataSet: Seq[Data],
+            initialCoefficients: Seq[Double],
+            loss: Seq[Double] => Double): Seq[Double]
 }
