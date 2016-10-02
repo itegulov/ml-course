438789151e30fb00fea119ceb8324d814b8c20db
alex-700 <alex_700_95@mail.ru>
2016-10-02 21:24:02 +0300

    add normalization

--- src/main/scala/ru/ifmo/ctddev/ml/hw2/Data.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw2/Data.scala
@@ -1,3 +1,24 @@
 package ru.ifmo.ctddev.ml.hw2
 
 case class Data(features: Seq[Double], answer: Double)
+
+object Data {
+  def normalize(list : Seq[Data]): Seq[Data] = {
+    def rec(lis : Seq[Seq[Double]]) : Seq[Seq[Double]] = {
+      if (lis.head.isEmpty) {
+        Seq()
+      } else {
+        normalizeDoubles(lis.map(_.head)) +: rec(lis.map(_.tail))
+      }
+    }
+    rec(list.map(_.features)).zip(normalizeDoubles(list.map(_.answer))).map{
+      case (list1, list2) => Data(list1, list2)
+    }
+  }
+
+  def normalizeDoubles(list : Seq[Double]) : Seq[Double] = {
+    val mean = list.sum / list.size
+    val sigma = list.map(x => (x - mean) * (x - mean)).sum
+    list.map(x => (x - mean) / sigma)
+  }
+}
