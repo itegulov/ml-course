374a46ea1279b9ec03f9859340a0939396b1402c
alex-700 <alex_700_95@mail.ru>
2016-10-02 21:56:24 +0300

    add GradientDescent

--- src/main/scala/ru/ifmo/ctddev/ml/hw2/GradientDescent.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw2/GradientDescent.scala
@@ -4,9 +4,23 @@ package ru.ifmo.ctddev.ml.hw2
   * Created by Aleksei Latyshev on 26.09.2016.
   */
 object GradientDescent extends LinearFitter {
-  override def fit(dataSet: Seq[Data],
-                     initialCoefficients: Seq[Double],
+  def cartesianProduct[T](xss: Seq[Seq[T]]): Seq[Seq[T]] = xss match {
+    case h +: t => for(xh <- h; xt <- cartesianProduct( t)) yield xh +: xt
+    case _ => Seq()
+  }
+  val firstStep : Double = 1
+  val eps : Double = 0.0005
+  override def fit(initialCoefficients: Seq[Double],
                      loss: Seq[Double] => Double): Seq[Double] = {
-    ???
+    def rec(ws : Seq[Double], step : Double) : Seq[Double] = {
+      if (step < eps) {
+        ws
+      } else {
+        rec(cartesianProduct(Seq.fill(ws.size)(Seq(-1, 0, 1))).map(_.zip(ws).map {
+          case (x, y) => x * step + y
+        }).minBy(loss), step / 2)
+      }
+    }
+    rec(initialCoefficients, firstStep)
   }
 }
