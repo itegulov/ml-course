c0e12e8f7a52109776fdc1551336be8beefeb538
Daniyar Itegulov <ditegulov@gmail.com>
2016-10-02 22:01:18 +0300

    Implement main

--- src/main/scala/ru/ifmo/ctddev/ml/hw2/Main.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw2/Main.scala
@@ -8,6 +8,18 @@ import breeze.plot._
 
 import scala.io.StdIn
 
-object Main extends App {
-  val initialSamples = HouseWithPrice.parseHouses(getClass.getResourceAsStream("/prices.txt"))
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val houses = HouseWithPrice.parseHouses(getClass.getResourceAsStream("/prices.txt"))
+    val data = Data.normalize(houses.map(_.toData))
+    val geneticCoefficients = GeneticAlgorithm.fit(Seq(0, 0), Utils.mseLoss(data))
+    println(s"Coefficients from genetic algorithm: ${geneticCoefficients.mkString(", ")}")
+    val geneticPredictor = LinearRegression(geneticCoefficients)
+    while (true) {
+      val area = StdIn.readInt()
+      val rooms = StdIn.readInt()
+      println("Genetic-predicted price: " + geneticPredictor(Seq(area, rooms)))
+    }
+  }
 }
--- src/main/scala/ru/ifmo/ctddev/ml/hw2/Utils.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw2/Utils.scala
@@ -5,4 +5,13 @@ package ru.ifmo.ctddev.ml.hw2
   */
 object Utils {
 
+  def mseLoss(data: Seq[Data])(coefficients: Seq[Double]): Double = {
+    val sum = data.map {
+      case Data(features, answer) =>
+        val predictedAnswer = coefficients.zip(features).map { case (f, s) => f * s }.sum
+        Math.pow(answer - predictedAnswer, 2)
+    }.sum
+
+    Math.pow(sum / data.length, 0.5)
+  }
 }
