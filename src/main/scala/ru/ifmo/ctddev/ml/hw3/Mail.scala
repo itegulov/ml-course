3ccc0df4aa416a487665d141b763025f2ee8f5f7
Daniyar Itegulov <ditegulov@gmail.com>
2016-10-21 17:40:53 +0300

    Add MailWithVerdict and implement parsing

--- src/main/scala/ru/ifmo/ctddev/ml/hw3/Mail.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw3/Mail.scala
@@ -1,12 +1,19 @@
 package ru.ifmo.ctddev.ml.hw3
 
-import java.io.File
+import java.io.InputStream
+import java.util.Scanner
 
 
 case class Mail(subject: Seq[Int], body: Seq[Int])
 
+case class MailWithVerdict(mail: Mail, verdict: Boolean)
+
 object Mail {
-  def parse(file: File): Mail = {
-    ???
+  def parse(is: InputStream): Mail = {
+    val scanner = new Scanner(is)
+    val subjectLine = scanner.nextLine().substring("Subject: ".length)
+    scanner.nextLine()
+    val bodyLine = scanner.nextLine()
+    Mail(subjectLine.split(' ').map(_.toInt), bodyLine.split(' ').map(_.toInt))
   }
 }
