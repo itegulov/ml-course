94662906c006723fb5ee779f27faf4a94f05ac12
alex-700 <alex_700_95@mail.ru>
2016-10-21 17:42:20 +0300

    add BayesClassifier interface

---
+++ src/main/scala/ru/ifmo/ctddev/ml/hw3/BayesClassifier.scala
@@ -0,0 +1,10 @@
+package ru.ifmo.ctddev.ml.hw3
+
+/**
+  * Created by Aleksei Latyshev on 21.10.2016.
+  */
+object BayesClassifier {
+  def train(mails : Seq[MailWithVerdict]) : Mail => Boolean = {
+
+  }
+}
