1e6f91e527736e6e2a64cd93529f3745226b708a
Daniyar Itegulov <ditegulov@gmail.com>
2016-11-05 23:38:21 +0300

    Add SVM interface

---
+++ src/main/scala/ru/ifmo/ctddev/ml/hw4/SVM.scala
@@ -0,0 +1,9 @@
+package ru.ifmo.ctddev.ml.hw4
+
+import ru.ifmo.ctddev.ml.hw1.{ PointWithClass, Point, PointClass }
+
+object SVM {
+  def train(trainSet: Seq[PointWithClass]): Point => PointClass = {
+    ???
+  }
+}
