e0eed6b674205ccec39a591b41f8c4c3f14e6c90
alex-700 <alex_700_95@mail.ru>
2016-11-11 16:28:26 +0300

    small fixes in lowess

--- src/main/scala/ru/ifmo/ctddev/ml/hw5/LowessRegression.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw5/LowessRegression.scala
@@ -7,14 +7,16 @@ import ru.ifmo.ctddev.ml.hw5.Kernels.Kernel
   */
 object LowessRegression {
   val eps = 1e-6
-  def getGammas(trainSet: Seq[Data], kernel: Kernel, windowSize: Double): Seq[Double] = {
+  def getGammas(trainSet: Seq[Data], kernel: Kernel, windowSize: Double, kernel2 : Kernel): Seq[Double] = {
     def rec(cur: Seq[Double]): Seq[Double] = {
-      val next = trainSet.zipWithIndex.map { case (di, i) =>
+      println(cur)
+      val a = trainSet.zipWithIndex.map { case (di, i) =>
         val k = trainSet.zipWithIndex.zip(cur).filter { case ((_, j), _) => i != j }.map {
           case ((data, _), gamma) => (gamma * kernel((data.x - di.x) / windowSize), data.y)
         }
         k.map { case (x, y) => x * y }.sum / k.map(_._1).sum
       }
+      val next = a.zip(trainSet.map(_.y)).map{ case (x, y) => kernel2(Math.abs(x - y)) }
       if (next.zip(cur).map{ case (x, y) => (x - y) * (x - y)}.sum < eps) {
         next
       } else {
--- src/main/scala/ru/ifmo/ctddev/ml/hw5/Main.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw5/Main.scala
@@ -2,12 +2,20 @@ package ru.ifmo.ctddev.ml.hw5
 
 import java.io.File
 
+import co.theasi.plotly._
+
 /**
   * Created by Aleksei Latyshev on 11.11.2016.
   */
 object Main extends App {
   val data = Data.parseData(new File(getClass.getResource("/non-parametric.csv").toURI))
-  val nWR = NadarayaWatsonRegression.train(data, Kernels.rectangleKernel, 1, Seq.fill(data.size)(1))
+  /*val plot = Plot().withScatter(data.map(_.x), data.map(_.y))
+  draw(plot, "my-second-plot")
+*/
+  val nWR = NadarayaWatsonRegression.train(data, Kernels.quarticKernel, 3, Seq.fill(data.size)(1))
+  /*val xs = 0D to 60D by 0.5
+  val plot2 = Plot().withScatter(xs, xs.map(nWR))
+  draw(plot2, "my-third-plot")*/
   val nWRWithLowess = NadarayaWatsonRegression.train(data, Kernels.rectangleKernel, 1,
-    LowessRegression.getGammas(data, Kernels.gaussKernel, 1))
+    LowessRegression.getGammas(data, Kernels.rectangleKernel, 1, Kernels.triangleKernel))
 }
