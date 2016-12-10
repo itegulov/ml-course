8717e1bec661fd23e535a6c45d4a3cd5958012ad
Daniyar Itegulov <ditegulov@gmail.com>
2016-12-10 22:51:19 +0300

    Implement file reading for HW7

--- src/main/scala/ru/ifmo/ctddev/ml/hw7/Data.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw7/Data.scala
@@ -1,5 +1,33 @@
 package ru.ifmo.ctddev.ml.hw7
 
-case class Data(grid: Array[Array[Double]])
+import java.io.{ DataInputStream, InputStream }
+
+case class Data(grid: IndexedSeq[IndexedSeq[Double]])
 
 case class DataWithAnswer(data: Data, answer: Int)
+
+object DataWithAnswer {
+  def loadFromFile(imagesIs: InputStream, labelsIs: InputStream): Seq[DataWithAnswer] = {
+    val imageDataIs = new DataInputStream(imagesIs)
+    val labelDataIs = new DataInputStream(labelsIs)
+    imageDataIs.readInt() // Magic number
+    labelDataIs.readInt() // Another magic number
+    val imagesCnt = imageDataIs.readInt()
+    require(labelDataIs.readInt() == imagesCnt, "There should be as many images as labels")
+    val rows = imageDataIs.readInt()
+    val cols = imageDataIs.readInt()
+    val dataMemes = for (_ <- 1 to imagesCnt) yield {
+      val image = for (_ <- 1 to rows)
+        yield
+          for (_ <- 1 to cols)
+            yield imageDataIs.readByte().toDouble / 255D
+      Data(image)
+    }
+    imageDataIs.close()
+    val answer = for (data <- dataMemes) yield {
+      DataWithAnswer(data, labelDataIs.readByte().toInt)
+    }
+    labelDataIs.close()
+    answer
+  }
+}
--- src/main/scala/ru/ifmo/ctddev/ml/hw7/Main.scala
+++ src/main/scala/ru/ifmo/ctddev/ml/hw7/Main.scala
@@ -2,6 +2,13 @@ package ru.ifmo.ctddev.ml.hw7
 
 object Main {
   def main(args: Array[String]): Unit = {
-
+    val trainData = DataWithAnswer.loadFromFile(
+      getClass.getResourceAsStream("/train-images-idx3-ubyte"),
+      getClass.getResourceAsStream("/train-labels-idx1-ubyte"))
+    val testData = DataWithAnswer.loadFromFile(
+      getClass.getResourceAsStream("/t10k-images-idx3-ubyte"),
+      getClass.getResourceAsStream("/t10k-labels-idx1-ubyte"))
+    println(trainData.size)
+    println(testData.size)
   }
 }
