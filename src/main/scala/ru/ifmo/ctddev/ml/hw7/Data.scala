1345cbdced3c0e51d04bf21e81cabe18fc4d1350
Daniyar Itegulov <ditegulov@gmail.com>
2016-12-10 21:50:33 +0300

    Add hw7

---
+++ src/main/scala/ru/ifmo/ctddev/ml/hw7/Data.scala
@@ -0,0 +1,5 @@
+package ru.ifmo.ctddev.ml.hw7
+
+case class Data(grid: Array[Array[Double]])
+
+case class DataWithAnswer(data: Data, answer: Int)
---
+++ src/main/scala/ru/ifmo/ctddev/ml/hw7/Main.scala
@@ -0,0 +1,7 @@
+package ru.ifmo.ctddev.ml.hw7
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+}
